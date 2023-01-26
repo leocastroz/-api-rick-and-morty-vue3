57c8a3a67833356ce94be7f341239bdd4f56f863
leocastroz <castroz.dev@gmail.com>
2023-01-26 10:28:01 -0300

    feat: pull API

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld/>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">API</RouterLink>
       </nav>
     </div>
   </header>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,61 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'leonardo',
+      action: {},
+      action2: {},
+    }
+  },
+  methods:{
+    /* primeira API */
+    pullAction(event){
+      const url = event.currentTarget.href
+      fetch(url)
+      .then(response => response.json())
+      .then(response =>  {
+        this.action = response
+      })
+    },
+    /* segunda API */
+    pullAction2(event){
+      const url = event.currentTarget.href
+      fetch(url)
+      .then(response => response.json())
+      .then(response =>  {
+        this.action2 = response
+      })
+    }
+  }
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/aapl/quote">Apple</a></li>
+    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/googl/quote">Google</a></li>
+    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/amzn/quote">Amazon</a></li>
+    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/ebay/quote">Ebay</a></li>
+    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/pypl/quote">Paypal</a></li>
+    <div class="">
+      Empresa: {{ action.symbol }} <br>
+      Comapanhia: {{ action.companyName }}
+    </div>
+    <br>
+    <div class="">
+      <li><a @click.prevent="pullAction2" href="https://rickandmortyapi.com/api/character/1">IMAGEM:</a></li>
+    </div>
+
+    <div>
+      <img :src="`${action2.image}`">
+      <br>
+      <br>
+    </div>
+
+    
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
