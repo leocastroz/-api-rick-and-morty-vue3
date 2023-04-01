18265137247bb87ebdf0ac840b2ed232e67933be
leocastroz <castroz.dev@gmail.com>
2023-04-01 11:57:39 -0300

    Refact: improve the code

---
+++ src/fetch.api/HowToFetch.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'castro',
+      action: {},
+      // action2: {},
+      // action3: {},
+    }
+  },
+  methods:{
+    getAction(){
+      fetch('https://api.github.com/users/leocastroz')
+      .then(response => response.json())
+      .then(data => {
+        this.action = data
+        console.log(this.action)
+      })
+    },
+    getAction2(){
+      fetch('https://api.github.com/users/leocastroz/repos')
+      .then(response => response.json())
+      .then(data => {
+        this.action2 = data
+        console.log(this.action2)
+      })
+    },
+    // getAction3(){
+    //   fetch('https://api.github.com/users/leonardocastro/followers')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     this.action3 = data
+    //     console.log(this.action3)
+    //   })
+    // },
+  },
+}
+
+</script>
+
+<template>
+  <div>
+    <p></p>
+    <button @click="getAction">Get Action</button>
+    <button @click="getAction2">Get Action2</button>
+    <!-- <button @click="getAction3">Get Action3</button> -->
+    <p>{{action.avatar_url}}</p>
+    <img :src="action.avatar_url" >
+    <p>{{action2}}</p>
+    <!-- <p>{{action3[0].login}}</p> -->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,61 +1,35 @@
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
-      name: 'leonardo',
-      action: {},
-      action2: {},
+      dog: null,
     }
   },
   methods:{
-    /* primeira API */
-    pullAction(event){
-      const url = event.currentTarget.href
-      fetch(url)
-      .then(response => response.json())
-      .then(response =>  {
-        this.action = response
-      })
+      async getDog() {
+      try {
+        const response = await axios.get('https://api.origamid.dev/stock/aapl/quote');
+        console.log(response.data);
+        this.dog = response.data;
+      } catch (error) {
+        console.error(error);
+      }
     },
-    /* segunda API */
-    pullAction2(event){
-      const url = event.currentTarget.href
-      fetch(url)
-      .then(response => response.json())
-      .then(response =>  {
-        this.action2 = response
-      })
-    }
-  }
+  },
 }
+
 </script>
 
 <template>
   <div>
-    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/aapl/quote">Apple</a></li>
-    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/googl/quote">Google</a></li>
-    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/amzn/quote">Amazon</a></li>
-    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/ebay/quote">Ebay</a></li>
-    <li><a @click.prevent="pullAction" href="https://api.origamid.dev/stock/pypl/quote">Paypal</a></li>
-    <div class="">
-      Empresa: {{ action.symbol }} <br>
-      Comapanhia: {{ action.companyName }}
-    </div>
-    <br>
-    <div class="">
-      <li><a @click.prevent="pullAction2" href="https://rickandmortyapi.com/api/character/1">IMAGEM:</a></li>
-    </div>
+    <button @click="getDog">Buscar cachorro</button>
+    <p>{{ dog }}</p>
 
-    <div>
-      <img :src="`${action2.image}`">
-      <br>
-      <br>
-    </div>
-
-    
   </div>
 </template>
 
-<style>
+<style scoped>
 
 </style>
